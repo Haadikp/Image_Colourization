9954526526ffed9cfa63f0e8ca5150d55d04d2b4
Hadi k p <hadikpkuniyil@gmail.com>
2025-05-13 21:16:32 +0530

    Updated app.py ports

--- project/app.py
+++ project/app.py
@@ -591,4 +591,5 @@ def logout():
     return redirect('/')  # Redirect to the login page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
